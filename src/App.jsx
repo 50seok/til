c07b226c11e43a0f16b723ea126e35112176ba85
50seok <dhdudtjr661@gmail.com>
2023-08-24 17:18:37 +0900

    any message

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  BrowserRouter,
 } from "react-router-dom";
 import Home from './routes/Home';
 import Detail from "./routes/Detail";
 function App() {
   return (
     <Router>
+      <BrowserRouter basename="/til/">
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/hello`} element={
           <h1>Hello!!</h1>
@@ -18,6 +20,7 @@ function App() {
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} >
         </Route>
       </Routes>
+      </BrowserRouter>
     </Router>
   );
 }
